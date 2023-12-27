6024e4703352eab76c12596c00b359994d47b400
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2023-12-27 15:24:38 +0200

    showing how to make a request with a variable and how we pass said variable

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,18 +1,43 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
-import { CategoriesContext } from '../../contexts/categories.context';
 import { CategoryContainer, Title } from './category.styles';
 import Spinner from '../../components/spinner/spinner.component';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle (title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category
+    }
+  });
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const { getCollectionsByTitle: { items} } = data;
+      setProducts(items);
+    }
+  }, [category, data]);
+
+  const [products, setProducts] = useState([]);
+
+  
 
   return (
     <Fragment>
