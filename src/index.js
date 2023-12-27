ecd074e040d425d22145eff6c94b53b06953912d
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2023-12-27 14:39:08 +0200

    added apollo client and graphql packages, initialized the apollo client and wrapped the app in a provider

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import './index.scss';
+
+const client = new ApolloClient({
+  uri: 'https://crwn-clothing.com/', // the entry point to our server (url that points to our server)
+  cache: new InMemoryCache()
+});
 
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   rootElement
 );
