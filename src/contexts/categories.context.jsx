f8b1fb58039b94907f933a4723e5e7249216dc5c
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2023-12-27 14:58:10 +0200

    implemented using querying data with graphql and a loading spinner

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -11,8 +11,10 @@ const COLLECTIONS = gql`
       id
       title
       items {
+        id
         name
         price
+        imageUrl
       }
     }
   }
@@ -22,9 +24,22 @@ export const CategoriesProvider = ({ children }) => {
   const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log(data);
+  console.log(loading);
 
-  const value = { categoriesMap };
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+
+      setCategoriesMap(collectionsMap);
+    }
+  }, [data]);
+
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
